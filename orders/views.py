55948348a5b5a8a9c7eb276af1f2e1d8316b0350
beppemiletto <beppe.miletto@gmail.com>
2023-02-10 17:22:52 +0100

    Begin orders app

---
+++ orders/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from carts.models import CartItem
+
+def place_order(request):
+    current_user = request.user
+
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+
+
+
+
+    return HttpResponse('Ok')
+
